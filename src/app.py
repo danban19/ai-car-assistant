b11707b73b634432c9f42d5f338f0fd081107a66
danban19 <daniel.banasik1@gmail.com>
2025-02-23 12:52:12 +0100

    implement azure openai integration

--- src/app.py
+++ src/app.py
@@ -1,6 +1,22 @@
 """Main entry point for the application."""
+import os
+
+from openai import AsyncAzureOpenAI
 import chainlit as cl
 
+
+client = AsyncAzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+)
+
+cl.instrument_openai()
+
+settings = {
+    "model": "gpt-4o-mini",
+    "temperature": 0
+}
 
 @cl.on_message
 async def main(message: cl.Message) -> None:
@@ -10,7 +26,17 @@ async def main(message: cl.Message) -> None:
     Returns:
         None
     """
-
-    await cl.Message(
-        content=f"Received: {message.content}",
-    ).send()
+    response = await client.chat.completions.create(
+        messages=[
+            {
+                "content": "You are a helpful bot.",
+                "role": "system"
+            },
+            {
+                "content": message.content,
+                "role": "user"
+            }
+        ],
+        **settings
+    )
+    await cl.Message(content=response.choices[0].message.content).send()
